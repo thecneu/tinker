b147915a19ad5fcb4492adc74dcb6bd475aac41b
corey.newhouse <Corey.Newhouse@deutschinc.com>
2017-03-03 10:47:05 -0800

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,36 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-header">
+        <header className="app-header">
           <img src={logo} className="app-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="app-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header>
+        <main>
+          <article>
+            <h1>Exercise</h1>
+            <form>
+              <input type="text" name="name"/>
+              <input type="text" name="musclegroup"/>
+              <input type="text" name="description"/>
+              <input type="text" name="reps"/>
+              <input type="text" name="sets"/>
+              <input type="file" name="image"/>
+              <input type="text" name="videourl"/>
+            </form>
+
+            <form>
+              <select name="type">
+                <option value="cardio">Cardio</option>
+                <option value="exercise">Exercise</option>
+              </select>
+              <input type="text" name="time"/>
+              <input type="text" name="distance"/>
+              <input type="text" name="calories"/>
+              <select name="exercise"></select>
+              <input type="text" name="weight"/>
+            </form>
+          </article>
+        </main>
       </div>
     );
   }
